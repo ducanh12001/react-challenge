9c70dc4f68474ecee84c51531b5081826185a3e4
ducanh12001 <70888594+ducanh12001@users.noreply.github.com>
2022-07-21 22:45:58 +0700

    ok

---
+++ rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
@@ -0,0 +1,28 @@
+import React, { useState, Component, memo } from 'react';
+import RocketCore from './RocketCore';
+
+export const FunctionalRocket = memo(() => {
+  const [initialLaunchTime] = useState(Date.now());
+
+  return <RocketCore initialLaunchTime={initialLaunchTime} />;
+})
+
+export class ClassRocket extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      initialLaunchTime: Date.now()
+    };
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.initialLaunchTime !== this.props.initialLaunchTime;
+  }
+
+  render() {
+    const { initialLaunchTime } = this.state;
+
+    return <RocketCore initialLaunchTime={initialLaunchTime} />;
+  }
+}
